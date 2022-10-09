7d732909e18b453b5a3beac782217afbbd525751
cesar.augusto <cesar.jose@stoom.com.br>
2022-10-09 19:52:52 -0300

    test: the final part of project testing

--- javascript-testing/aula05-tdd-project-pt03/src/usecases/rentCar.js
+++ javascript-testing/aula05-tdd-project-pt03/src/usecases/rentCar.js
@@ -1,12 +1,14 @@
 const CarCategory = require("../entities/carCategory");
 const Customer = require("../entities/customer");
 const Tax = require("../entities/tax");
+const Transaction = require("../entities/transaction");
 const BaseRepository = require("../repository/base/baseRepository");
 
 class RentCar {
-  constructor(carCategories, customers) {
+  constructor(carCategories, customers, cars) {
     this.carCategoriesRepository = new BaseRepository({ file: carCategories });
     this.customersRepository = new BaseRepository({ file: customers });
+    this.carRepository = new BaseRepository({ file: cars });
   }
 
   async execute(days, customerId, carCartegoryId) {
@@ -23,6 +25,50 @@ class RentCar {
     const value = tax.calculateTheFee(customer.age, carCategory.price, days);
 
     return value;
+  }
+
+  getRandomCarFromCategory(data) {
+    const listLength = data.length;
+    return Math.floor(Math.random() * listLength);
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomCarFromCategory(carCategory.carIds);
+
+    const carId = carCategory.carIds[randomCarIndex];
+
+    return carId;
+  }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    const car = await this.carRepository.find(carId);
+
+    return car;
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.getAvailableCar(carCategory);
+
+    const tax = new Tax();
+
+    const amount = tax.calculateTheFee(
+      customer.age,
+      carCategory.price,
+      numberOfDays
+    );
+
+    const today = new Date();
+
+    today.setDate(today.getDate() + numberOfDays);
+
+    const options = { year: "numeric", month: "long", day: "numeric" };
+
+    const dueDate = today.toLocaleDateString("pt-br", options);
+
+    const transaction = new Transaction({ customer, car, amount, dueDate });
+
+    return transaction;
   }
 }
 
--- javascript-testing/aula05-tdd-project-pt03/tests/useCases/rentCar.test.js
+++ javascript-testing/aula05-tdd-project-pt03/tests/useCases/rentCar.test.js
@@ -4,22 +4,15 @@ const { describe, it, before } = require("mocha");
 const { expect } = require("chai");
 const Tax = require("../../src/entities/tax");
 const sinon = require("sinon");
+const Transaction = require("../../src/entities/transaction");
 const carCategoriesDatabase = join(
   __dirname,
-  "../",
-  "../",
-  "database",
+  "./../../database",
   "carCategories.json"
 );
 
-const customersDatabase = join(
-  __dirname,
-  "../",
-  "../",
-  "database",
-  "customers.json"
-);
-
+const customersDatabase = join(__dirname, "./../../database", "customers.json");
+const carsDatabase = join(__dirname, "./../../database", "cars.json");
 const mocks = {
   validCarCategory: require("./../mocks/valid-carCategory.json"),
 
@@ -29,11 +22,12 @@ const mocks = {
 };
 
 describe("Use case - Rent a car", () => {
+  /**@type {RentCar} */
   let sut = {};
   let sandbox = {};
 
   before(() => {
-    sut = new RentCar(carCategoriesDatabase, customersDatabase);
+    sut = new RentCar(carCategoriesDatabase, customersDatabase, carsDatabase);
   });
 
   beforeEach(() => {
@@ -44,8 +38,51 @@ describe("Use case - Rent a car", () => {
     sandbox.restore();
   });
 
+  it("Should return a random index from an array", () => {
+    const data = [0, 1, 2, 3, 4];
+
+    const result = sut.getRandomCarFromCategory(data);
+
+    expect(result).to.be.lte(result).and.be.gte(0);
+  });
+
+  it("Should choose the first id from carIds in carCategory", () => {
+    const carCategory = mocks.validCarCategory;
+
+    const carIdIndex = 0;
+
+    sandbox.stub(sut, sut.getRandomCarFromCategory.name).returns(carIdIndex);
+
+    const result = sut.chooseRandomCar(carCategory);
+
+    const expected = carCategory.carIds[carIdIndex];
+
+    expect(sut.getRandomCarFromCategory.calledOnce).to.be.ok;
+    expect(result).to.be.equal(expected);
+  });
+
+  it("Given a carCategory it should return an avaiable car", async () => {
+    const car = mocks.validCar;
+
+    const carCategory = Object.create(mocks.validCarCategory);
+
+    carCategory.carIds = [car.id];
+
+    sandbox.stub(sut.carRepository, sut.carRepository.find.name).resolves(car);
+
+    sandbox.spy(sut, sut.chooseRandomCar.name);
+
+    const result = await sut.getAvailableCar(carCategory);
+    const expected = car;
+
+    expect(sut.chooseRandomCar.calledOnce).to.be.ok;
+
+    expect(sut.carRepository.find.calledWithExactly(car.id)).to.be.ok;
+    expect(result).to.be.deep.equal(expected);
+  });
+
   it("Should calculate the fee based on age major than 30", async () => {
-    const customer = mocks.validCustomer[0];
+    const customer = mocks.validCustomer;
 
     const carCategory = mocks.validCarCategory;
     sandbox
@@ -66,7 +103,9 @@ describe("Use case - Rent a car", () => {
   });
 
   it("Should calculate the fee based on age between 18 and 25 ", async () => {
-    const customer = mocks.validCustomer[1];
+    const customer = Object.create(mocks.validCustomer);
+
+    customer.age = 24;
 
     const carCategory = mocks.validCarCategory;
     sandbox
@@ -87,7 +126,9 @@ describe("Use case - Rent a car", () => {
   });
 
   it("Should calculate the fee based on age between 26 and 30 ", async () => {
-    const customer = mocks.validCustomer[2];
+    const customer = Object.create(mocks.validCustomer);
+
+    customer.age = 28;
 
     const carCategory = mocks.validCarCategory;
     sandbox
@@ -104,6 +145,44 @@ describe("Use case - Rent a car", () => {
 
     const expected = tax.formatTheTax(282);
 
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("given a customer and a car category it should return a transaction data", async () => {
+    const customer = Object.create(mocks.validCustomer);
+
+    const car = mocks.validCar;
+
+    customer.age = 20;
+
+    const carCategory = {
+      ...mocks.validCarCategory,
+      price: 37.6,
+      carIds: [car.id],
+    };
+
+    const numberOfDays = 5;
+
+    const dueDate = "10 de novembro de 2020";
+
+    const now = new Date(2020, 10, 5);
+
+    sandbox.useFakeTimers(now.getTime());
+
+    sandbox.stub(sut.carRepository, sut.carRepository.find.name).resolves(car);
+
+    const tax = new Tax();
+
+    const amount = tax.formatTheTax(206.8);
+
+    const expected = new Transaction({
+      customer,
+      car,
+      amount,
+      dueDate,
+    });
+
+    const result = await sut.rent(customer, carCategory, numberOfDays);
     expect(result).to.be.deep.equal(expected);
   });
 });
